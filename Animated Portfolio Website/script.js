f4b55f185612c9ed95e3101dd502ec51d249beef
Pankaj <pankaj.summerproject2016@gmail.com>
2024-04-05 13:12:14 +0530

    scroll segments

--- Animated Portfolio Website/script.js
+++ Animated Portfolio Website/script.js
@@ -1,7 +1,61 @@
-    var typed = new Typed('.multiple-text', {
-      strings: ["Web Developer^3000", "Python 3 Programmer", "MERN Stack Developer"],
-      typeSpeed: 100,
-      backSpeed: 100,
-      backDelay: 1000,
-      loop: true
-    });
+var typed = new Typed('.multiple-text', {
+  strings: ["Web Developer^3000", "Python 3 Programmer", "MERN Stack Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+
+// toggle icon navbar
+
+
+let menuIcon = document.querySelector('#menu-icon')
+let navbar = document.querySelector('.navbar')
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle('bx_bx-x')
+  navbar.classList.toggle('active')
+}
+
+
+// Section Scroll Control
+
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a')
+
+window.onscroll = () => {
+  sections.forEach(sec => {
+    let top = window.scrollY
+    let offset = sec.offsetTop - 150
+    let height = sec.offsetHeight;
+    let id = sec.getAttribute('id')
+
+    if (top >= offset && top < offset + height) {
+      navLinks.forEach.apply(links => {
+        links.classList.remove('active')
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active')
+      })
+    }
+  })
+}
+
+// Sticky Navbar
+let header = document.querySelector('header')
+header.classList.toggle('sticky', window.scrollY > 100)
+
+// remove toggle icon and navbar
+menuIcon.classList.remove('bx_bx-x')
+navbar.classList.remove('active')
+
+ScrollReveal({
+  distance: '80px',
+  duration: 2000,
+  delay: 200
+})
+
+ScrollReveal().reveal('.home-content, heading', { origin: 'top' })
+ScrollReveal().reveal('.home-img, .skills-container, .portfolio-box , .contact form ', { origin: 'button' })
+ScrollReveal().reveal('.home-contact h1, .about-img', { origin: 'left' })
+ScrollReveal().reveal('.home-contact p, .about', { origin: 'right' })
+
+
